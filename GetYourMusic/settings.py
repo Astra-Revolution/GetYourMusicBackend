ab94d6b1eb725fae65feac51311363ee1496087c
Sebastian <sebastian.noli@gmail.com>
2021-03-28 12:04:37 -0500

    local settings

--- GetYourMusic/settings.py
+++ GetYourMusic/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'get_your_music',
         'USER': 'root',
-        'PASSWORD': 'pacheco98',
+        'PASSWORD': 's-noli457',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
