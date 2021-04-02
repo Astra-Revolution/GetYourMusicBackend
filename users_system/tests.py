80475b46662c94d109ef04bce1b091d4cc606ec3
MarioTataje <magotor1304@gmail.com>
2021-04-02 02:15:09 -0500

    added filter test

--- users_system/tests.py
+++ users_system/tests.py
@@ -203,6 +203,12 @@ class MusicianTest(APITestCase):
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_filter_musicians(self):
+        response = self.client.post(reverse('musician_filter'),
+                                    data=json.dumps({}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_create_valid_musicians_genres(self):
         response = self.client.post(
             reverse('musicians_genres', kwargs={'musician_id': self.noli_musician.id,
