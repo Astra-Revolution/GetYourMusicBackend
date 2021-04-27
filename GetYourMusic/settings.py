0cbe20e8451a463082fa98c1aa005662ef4939b4
DAlembert721 <dalembert.dmu@gmail.com>
2021-04-27 01:54:41 -0500

    Set new storage

--- GetYourMusic/settings.py
+++ GetYourMusic/settings.py
@@ -18,9 +18,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 STATICFILES_STORAGE = 'GetYourMusic.storage.S3Storage'
 
