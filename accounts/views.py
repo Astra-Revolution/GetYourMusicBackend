a923277cb19e5cf0aaa7d75d2116a0d7140d99e6
MarioTataje <magotor1304@gmail.com>
2021-05-30 21:42:14 -0500

    refactor: updated musician filter

--- accounts/views.py
+++ accounts/views.py
@@ -151,7 +151,8 @@ def musicians_list(request):
 
 
 @swagger_auto_schema(method='get', responses={200: musician_response})
-@api_view(['GET'])
+@swagger_auto_schema(methods=['patch'], request_body=MusicianSerializer)
+@api_view(['GET', 'PATCH'])
 @permission_classes([IsAuthenticated])
 def musicians_detail(request, musician_id):
     try:
@@ -162,6 +163,13 @@ def musicians_detail(request, musician_id):
     if request.method == 'GET':
         serializer = MusicianSerializer(musician)
         return Response(serializer.data)
+
+    elif request.method == 'PATCH':
+        serializer = MusicianSerializer(musician, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @swagger_auto_schema(method='get', responses={200: organizers_response})
