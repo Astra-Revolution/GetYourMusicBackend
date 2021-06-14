0f232f8e577ce60f3106ed13a083b378a00b2a3c
DAlembert721 <dalembert.dmu@gmail.com>
2021-06-14 03:14:03 -0500

    feat: Add create chat room endpoint

--- messaging/serializers.py
+++ messaging/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from accounts.models import Profile
 from .models import Chat
 
 
@@ -18,6 +20,16 @@ class ChatSerializer(serializers.ModelSerializer):
         full_name = f'{receiver.first_name} {receiver.last_name}'
         return full_name
 
+    def create(self, validated_data):
+        sender = Profile.objects.get(user_id=validated_data["sender_id"])
+        receiver = Profile.objects.get(user_id=validated_data["receiver_id"])
+        chat = Chat.objects.filter(sender=receiver, receiver=sender)
+        if chat:
+            return chat[0]
+        chat = Chat.objects.get_or_create(sender=sender, receiver=receiver)
+        return chat[0]
+
     class Meta:
         model = Chat
         fields = ('id', 'sender_id', 'receiver_id', 'sender_name', 'receiver_name')
+        read_only_fields = ('sender_id', 'receiver_id')
--- messaging/urls.py
+++ messaging/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from messaging.views import list_chats_by_user
+from messaging.views import *
 
 urlpatterns = [
     path('profiles/<int:user_id>/chats/', list_chats_by_user, name='list_chats_by_user'),
+    path('profiles/<int:user_id>/receivers/<int:receiver_id>/chats/', chat_detail, name='chat_detail'),
 ]
 
 # application = ProtocolTypeRouter({
